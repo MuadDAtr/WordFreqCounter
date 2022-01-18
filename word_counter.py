5e82b533f617c3e9b10d3f215ebb70237da41a19
Paul <int.stron@protonmail.com>
2022-01-18 04:50:15 +0100

    Correcting the main function

--- word_counter.py
+++ word_counter.py
@@ -1,21 +1,25 @@
 from modules import char_replace, count_freq, freq_dict, sort_dict
 
-text_init = input('Please insert the text: ')
-print("\n")
-print(text_init)
+def main():
+    text_init = input('Please insert the text: ')
+    print("\n")
+    print(text_init)
 
-text_l= text_init.lower()
+    text_l= text_init.lower()
 
-text = char_replace(text_l)
+    text = char_replace(text_l)
 
-wordlist = text.split()
+    wordlist = text.split()
 
-count_freq(wordlist)
-print("...............")
-print("\n")
-print("\n")
-print("This is the frequency of words in the text: \n")
+    count_freq(wordlist)
+    print("...............")
+    print("\n")
+    print("\n")
+    print("This is the frequency of words in the text: \n")
 
-freqdict = freq_dict(wordlist)
+    freqdict = freq_dict(wordlist)
 
-print(sort_dict(freqdict))
+    print(sort_dict(freqdict))
+
+if __name__ == "__main__":
+    main()
