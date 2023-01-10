73994b4c853a2b11257fb3651e6c39201e8c1afd
MuadDAtr <int.stron@protonmail.com>
2023-01-11 00:07:24 +0100

    Adding interface

--- interface.py
+++ interface.py
@@ -7,7 +7,7 @@ class FreqQUI:
         self.label = tk.Label(self.root, text = "Word Frequency Counter", font = ('Times New Roman', 18))
         self.label.pack(padx = 20, pady =20)
 
-
+        self.textbox = 
         self.root.mainloop()
 
 
