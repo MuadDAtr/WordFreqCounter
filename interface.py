8d61e428802d202abde99fb4cd829f799f670a91
MuadDAtr <int.stron@protonmail.com>
2023-01-10 23:55:35 +0100

    Adding GUI

---
+++ interface.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+
+class FreqQUI:
+    def __init__(self):
+        self.root = tk.Tk()
+        
+        self.label = tk.Label(self.root, text = "Word Frequency Counter", font = ('Times New Roman', 18))
+        self.label.pack(padx = 20, pady =20)
+
+
+        self.root.mainloop()
+
+
+FreqQUI()
