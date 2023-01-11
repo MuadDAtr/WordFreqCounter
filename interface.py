ba533d323dfbf9c026debada4e3349b173bda75a
MuadDAtr <int.stron@protonmail.com>
2023-01-11 04:09:14 +0100

    textbox, button

--- interface.py
+++ interface.py
@@ -7,7 +7,12 @@ class FreqQUI:
         self.label = tk.Label(self.root, text = "Word Frequency Counter", font = ('Times New Roman', 18))
         self.label.pack(padx = 20, pady =20)
 
-        self.textbox = 
+        self.textbox = tk.Text(self.root, height = 10, font = ('Arial', 12))
+        self.textbox.pack(padx = 10)
+
+        self.button = tk.Button(self.root, text = "Count", font = ('Arial', 14))
+        self.button.pack(padx = 10, pady = 10)
+
         self.root.mainloop()
 
 
